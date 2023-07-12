ce4c2e916ae2e0ebe4921f76f2fdccc30f9e16e9
Yuu528 <yuu528mail@gmail.com>
2023-07-12 13:20:14 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+LOCAL_BASHRC="$HOME/.bashrc"
+LOCAL_CONFIG="$HOME/.config"
+REPO_BASHRC="./.bashrc"
+REPO_CONFIG="$(cd $(dirname $0); pwd)/.config"
+
+function askyn() {
+	local prompt=""
+	local rev=0
+	case $# in
+		0)
+			prompt="[y/N]: ";;
+
+		1)
+			prompt="$1 [y/N]: ";;
+
+		2)
+			case $1 in
+				"-y")
+					prompt="$2 [Y/n]"
+					rev=1;;
+
+				*)
+					prompt="$2 [y/N]";;
+			esac;;
+	esac
+	read -n1 -p "$prompt" yn
+
+	case "$yn" in
+		"")
+			if [ $rev -eq 1 ]; then
+				true
+			else
+				false
+			fi;;
+
+		[yY]*)
+			echo ""
+			true;;
+
+		*)
+			echo ""
+			false;;
+	esac
+}
+
+function mvbak() {
+	if [ -e "$1" ]; then
+		mv -bi "$1" "$1.bak"
+	fi
+}
+
+function mklink() {
+	pushd "$LOCAL_CONFIG"
+	ln -s "$REPO_CONFIG/$1" "$1"
+	popd
+}
+
+echo "First time setup wizard of dotfiles"
+
+askyn "Install .bashrc?" && {
+	mvbak "$LOCAL_BASHRC"
+	ln -s "$REPO_BASHRC" "$LOCAL_BASHRC"
+}
+
+askyn "Install CUI app configs?" && {
+	mvbak "$LOCAL_CONFIG/coc"
+	mvbak "$LOCAL_CONFIG/nvim"
+	mvbak "$LOCAL_CONFIG/powerline-shell"
+	mklink coc
+	mklink nvim
+	mklink powerline-shell
+}
+
+askyn "Install GUI configs?" && {
+	mvbak "$LOCAL_CONFIG/alacritty"
+	mvbak "$LOCAL_CONFIG/i3"
+	mklink alacritty
+	mklink i3
+}
+
+echo "Complete! Bye!"
