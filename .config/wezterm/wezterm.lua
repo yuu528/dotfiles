81896522d938b2347d3a166c5d553c69f1835018
Yuu528 <yuu528mail@gmail.com>
2023-11-26 17:37:56 +0900

    WezTerm: remove window padding

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,10 +14,16 @@ config.font_size = 10
 config.color_scheme = 'GruvboxDark'
 config.bold_brightens_ansi_colors = "No"
 
--- Window Bars
+-- Window
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0
+}
 
 -- Default Shell
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
