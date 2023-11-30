5d79fbcc53bd6fa8801c833b2e9c4685953e9b8b
yuu528 <yuuta.kawaguchi28@gmail.com>
2023-11-30 12:33:48 +0900

    Neovim: update lsp settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -55,25 +55,17 @@ vim.g.maplocalleader = ','
 vim.keymap.set('n', 'j', 'gj')
 vim.keymap.set('n', 'k', 'gk')
 
--- builtin LSP
-vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
-vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
-vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
-
-
 -- LSP Handler
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
 )
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 
 -- autocmds
 -- filetype indent settings
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
     {
         'neovim/nvim-lspconfig',
-        lazy = false
+        lazy = false,
     },
     {
         'williamboman/mason.nvim',
@@ -13,7 +13,7 @@ return {
         lazy = false
     },
     {
-        'hrsh7th/cmp-cmdline',
+        'L3MON4D3/LuaSnip',
         lazy = false
     },
     {
@@ -21,10 +21,6 @@ return {
         dependencies = {
             'L3MON4D3/LuaSnip'
         },
-        lazy = false
-    },
-    {
-        'L3MON4D3/LuaSnip',
         lazy = false
     },
     {
@@ -77,7 +73,27 @@ return {
             require 'mason-lspconfig'.setup_handlers {
                 function(server)
                     require('lspconfig')[server].setup {
-                        capabilities = require 'cmp_nvim_lsp'.default_capabilities()
+                        capabilities = require 'cmp_nvim_lsp'.default_capabilities(),
+                        on_attach = function(_, bufnr)
+                            local bufopts = {silent = true, buffer = bufnr}
+                            vim.keymap.set('n', 'K',  vim.lsp.buf.hover(), bufopts)
+                            vim.keymap.set('n', 'gf', vim.lsp.buf.formatting(), bufopts)
+                            vim.keymap.set('n', 'gr', vim.lsp.buf.references(), bufopts)
+                            vim.keymap.set('n', 'gd', vim.lsp.buf.definition(), bufopts)
+                            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration(), bufopts)
+                            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation(), bufopts)
+                            vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition(), bufopts)
+                            vim.keymap.set('n', 'gn', vim.lsp.buf.rename(), bufopts)
+                            vim.keymap.set('n', 'ga', vim.lsp.buf.code_action(), bufopts)
+                            vim.keymap.set('n', 'ge', vim.diagnostic.open_float(), bufopts)
+                            vim.keymap.set('n', 'g]', vim.diagnostic.goto_next(), bufopts)
+                            vim.keymap.set('n', 'g[', vim.diagnostic.goto_prev(), bufopts)
+                        end,
+                        diagnostics = {
+                            virtual_text = {
+                                prefix = 'icons'
+                            }
+                        }
                     }
                 end
             }
@@ -122,7 +138,6 @@ return {
                 },
                 sources = {
                     {name = 'buffer'},
-                    {name = 'cmdline'},
                     {name = 'luasnip'},
                     {name = 'nvim_lsp'},
                     {name = 'path'}
