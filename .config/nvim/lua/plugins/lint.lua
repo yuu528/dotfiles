bf959e9c7ca47b3f20a0fd60b8dcf9498292b4fc
yuu528 <yuuta.kawaguchi28@gmail.com>
2023-12-04 12:49:47 +0900

    Neovim: Add mason-nvim-lint

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -1,11 +1,11 @@
 return {
     {
         'mfussenegger/nvim-lint',
-        event = {'BufRead', 'BufNewFile'},
+        event = { 'BufRead', 'BufNewFile' },
         config = function()
-            vim.api.nvim_create_augroup('Linting', {clear = true})
+            vim.api.nvim_create_augroup('Linting', { clear = true })
             vim.api.nvim_create_autocmd(
-                {'BufEnter', 'BufWritePost', 'InsertLeave'},
+                { 'BufEnter', 'BufWritePost', 'InsertLeave' },
                 {
                     group = 'Linting',
                     callback = function()
@@ -14,14 +14,29 @@ return {
                 }
             )
             require 'lint'.linters_by_ft = {
-                javascript = {'eslint_d'},
-                typescript = {'eslint_d'},
-                python = {'pylint'},
-                html = {'tidy'},
-                lua = {'luacheck'},
-                markdown = {'markdownlint'},
-                c = {'clangtidy'}
+                javascript = { 'eslint_d' },
+                typescript = { 'eslint_d' },
+                python = { 'pylint' },
+                html = { 'tidy' },
+                lua = { 'luacheck' },
+                markdown = { 'markdownlint' },
+                c = { 'clangtidy' }
             }
         end
+    },
+    {
+        'rshkarin/mason-nvim-lint',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'mfussenegger/nvim-lint'
+        },
+        event = { 'BufReadPre', 'BufNewFile' },
+        config = true,
+        opts = {
+            ensure_installed = {
+                'eslint_d', 'pylint', 'tidy', 'luacheck', 'markdownlint', 'clangtidy'
+            }
+        }
     }
 }
+
