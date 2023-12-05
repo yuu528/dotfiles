0999a8adc2b5f6dbc665701fd8a722102d1245b0
yuu528 <yuuta.kawaguchi28@gmail.com>
2023-12-05 09:22:54 +0900

    Neovim: nvim-tree: change icons

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -50,6 +50,30 @@ return {
                 },
                 renderer = {
                     group_empty = true,
+                    icons = {
+                        glyphs = {
+                            folder = {
+                                arrow_closed = '',
+                                arrow_open = '',
+                                default = '',
+                                open = '',
+                                empty = '',
+                                empty_open = '',
+                                symlink = '',
+                                symlink_open = '',
+
+                            },
+                            git = {
+                                unstaged = '',
+                                staged = '',
+                                unmerged = '',
+                                renamed = '',
+                                untracked = '',
+                                deleted = '',
+                                ignored = ''
+                            }
+                        }
+                    }
                 },
                 filters = {
                     dotfiles = false
