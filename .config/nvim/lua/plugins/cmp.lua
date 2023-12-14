d934ad6549924e23857f0562ceb8d0d3a363d2e3
Yuu528 <yuu528mail@gmail.com>
2023-12-14 23:04:04 +0900

    Neovim: change mapping for LuaSnip/nvim-cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -34,8 +34,6 @@ return {
                     ['<TAB>'] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
                         else
                             fallback()
                         end
@@ -43,8 +41,6 @@ return {
                     ['<S-Tab>'] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
                         else
                             fallback()
                         end
