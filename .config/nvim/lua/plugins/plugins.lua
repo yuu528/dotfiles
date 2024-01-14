276d314fd5563da642662e001517168452ebd858
Yuu528 <yuu528mail@gmail.com>
2024-01-14 18:09:11 +0900

    Neovim: Add copilot.vim

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -30,6 +30,20 @@ return {
         config = true
     },
     {
+        'github/copilot.vim',
+        cmd = 'Copilot',
+        event = {
+            'BufRead', 'BufNewFile'
+        },
+        config = function()
+            vim.g.copilot_no_tab_map = true
+            vim.keymap.set('i', '<C-i>', 'copilot#Accept("\\<CR>")', {
+                expr = true,
+                replace_keycodes = false
+            })
+        end
+    },
+    {
         'mattn/emmet-vim',
         ft = { 'html', 'css' }
     },
