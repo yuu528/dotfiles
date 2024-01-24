756393862c652dd87f3fddd4613654c127ab6f05
Yuu528 <yuu528mail@gmail.com>
2024-01-24 11:34:29 +0900

    Neovim: nvim-cmp: change esc mapping to C-e

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -58,7 +58,7 @@ return {
                         s = cmp.mapping.confirm({ select = true }),
                         c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
                     },
-                    ['<ESC>'] = cmp.mapping(function(fallback)
+                    ['<C-e>'] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.abort()
                         else
