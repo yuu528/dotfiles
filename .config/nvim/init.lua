d93e9e457c5715fb7312eb89a6ed809771f86f32
Yuu528 <yuu528mail@gmail.com>
2024-03-29 15:26:29 +0900

    Neovim: Avoid loading plugins on small OSes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -127,23 +127,35 @@ end, {
     }
 )
 
+-- get os
+local os = 'unknown'
+local arch = 'unknown'
+
+if vim.fn.has('win32') or vim.fn.has('win64') then
+    os = 'win'
+elseif vim.fn.executable('uname') then
+    arch = vim.fn.system('uname -m')
+end
+
 -- load lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
+-- (only on win, x64. don't load plugins on raspi)
+if os == 'win' or arch == 'x86_64' then
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system({
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath,
+        })
+    end
+    vim.opt.rtp:prepend(lazypath)
+
+    require 'lazy'.setup('plugins', {
+        defaults = {
+            lazy = true
+        }
     })
 end
-vim.opt.rtp:prepend(lazypath)
-
-require 'lazy'.setup('plugins', {
-    defaults = {
-        lazy = true
-    }
-})
-
