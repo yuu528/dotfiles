b775cd2ddd4c6b0989b29d603ee9d921f2637951
yuu528 <yuuta.kawaguchi28@gmail.com>
2024-05-31 13:24:00 +0900

    Neovim: Fix jdtls

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,8 +1,15 @@
 local config = {
-    cmd = {
-        vim.fn.stdpath('data') .. '/mason/bin/jdtls'
-    },
     root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
 }
+
+if vim.fn.has('win32') == 1 then
+    config.cmd = {
+        vim.fn.expand(vim.fn.stdpath('data') .. '/mason/bin/jdtls.cmd')
+    }
+else
+    config.cmd = {
+        vim.fn.expand(vim.fn.stdpath('data') .. '/mason/bin/jdtls')
+    }
+end
 
 require 'jdtls'.start_or_attach(config)
