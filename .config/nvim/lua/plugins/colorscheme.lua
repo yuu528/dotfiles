f99bdf5794cd675ad37bbfc90124563d3b375727
Yuu528 <yuu528mail@gmail.com>
2024-07-28 18:32:21 +0900

    Neovim: Add vim-polar

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        'ellisonleao/gruvbox.nvim',
+        priority = 1000,
+        lazy = false,
+        config = function()
+            require 'gruvbox'.setup({
+                overrides = {
+                    SignColumn = { link = 'Normal' },
+                    GruvboxGreenSign = { bg = '' },
+                    GruvboxOrangeSign = { bg = '' },
+                    GruvboxPurpleSign = { bg = '' },
+                    GruvboxYellowSign = { bg = '' },
+                    GruvboxRedSign = { bg = '' },
+                    GruvboxBlueSign = { bg = '' },
+                    GruvboxAquaSign = { bg = '' },
+                },
+            })
+            vim.o.background = 'dark'
+            vim.cmd('colorscheme gruvbox')
+        end
+    },
+    {
+        'habamax/vim-polar',
+        priority = 1000,
+        lazy = false
+    }
+}
--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -103,27 +103,6 @@ return {
         config = true
     },
     {
-        'ellisonleao/gruvbox.nvim',
-        priority = 1000,
-        lazy = false,
-        config = function()
-            require 'gruvbox'.setup({
-                overrides = {
-                    SignColumn = { link = 'Normal' },
-                    GruvboxGreenSign = { bg = '' },
-                    GruvboxOrangeSign = { bg = '' },
-                    GruvboxPurpleSign = { bg = '' },
-                    GruvboxYellowSign = { bg = '' },
-                    GruvboxRedSign = { bg = '' },
-                    GruvboxBlueSign = { bg = '' },
-                    GruvboxAquaSign = { bg = '' },
-                },
-            })
-            vim.o.background = 'dark'
-            vim.cmd('colorscheme gruvbox')
-        end
-    },
-    {
         'NMAC427/guess-indent.nvim',
         event = { 'BufRead', 'BufNewFile' },
         config = true,
