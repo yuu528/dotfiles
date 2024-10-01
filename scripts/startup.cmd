2a01ff4fd79d19514161ffa11439460f260de2ad
yuu528 <yuuta.kawaguchi28@gmail.com>
2024-10-01 11:18:57 +0900

    komorebi-bar: Launch the bar on startup when single monitor env

--- scripts/startup.cmd
+++ scripts/startup.cmd
@@ -2,7 +2,15 @@
 
 set KOMOREBI_CONFIG_HOME=%USERPROFILE%\.config\komorebi
 
-komorebic start --whkd
+for /f "delims=" %%i in ('powershell -Command "@(Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorBasicDisplayParams).Length"') do set monitor=%%i
 
-cd "%USERPROFILE%\git\yuu528\yasb"
-python src/main.py
+if %monitor% == 1 (
+	rem for single monitor
+	komorebic start --whkd --bar
+) else (
+	rem for multi monitor
+	komorebic start --whkd
+
+	cd "%USERPROFILE%\git\yuu528\yasb"
+	python src/main.py
+)
