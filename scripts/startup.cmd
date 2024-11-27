ed275121d3594d60a67d6ab8983098f43831ff99
Yuu528 <yuu528mail@gmail.com>
2024-11-27 12:12:41 +0900

    startup: Remove multi monitor check

--- scripts/startup.cmd
+++ scripts/startup.cmd
@@ -2,15 +2,4 @@
 
 set KOMOREBI_CONFIG_HOME=%USERPROFILE%\.config\komorebi
 
-for /f "delims=" %%i in ('powershell -Command "@(Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorBasicDisplayParams).Length"') do set monitor=%%i
-
-if %monitor% == 1 (
-	rem for single monitor
-	komorebic start --whkd --bar
-) else (
-	rem for multi monitor
-	komorebic start --whkd
-
-	cd "%USERPROFILE%\git\yuu528\yasb"
-	python src/main.py
-)
+komorebic start --whkd --bar
