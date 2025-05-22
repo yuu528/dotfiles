40e4b69f31690f469655f518573e30c61b78c545
yuu528 <yuuta.kawaguchi28@gmail.com>
2025-05-22 10:53:52 +0900

    neovim: Update for Mason v2

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,88 +2,69 @@ return {
     'mfussenegger/nvim-jdtls',
     'mfussenegger/nvim-lint',
     'rshkarin/mason-nvim-lint',
+    'mason-org/mason.nvim',
     {
-        'williamboman/mason-lspconfig.nvim',
-        tag = 'v1.32.0'
-    },
-    {
-        'williamboman/mason.nvim',
-        tag = 'v1.11.0'
-    },
-    {
-        'neovim/nvim-lspconfig',
+        'mason-org/mason-lspconfig.nvim',
         lazy = false,
+        dependencies = {
+            'mason-org/mason.nvim',
+            'neovim/nvim-lspconfig',
+        },
         config = function()
-            local function setup_lsp()
-                require 'mason'.setup()
+            -- ts_ls config
+            local vue_ts_plugin = vim.fn.expand(
+                '$MASON/packages/vue-language-server/node_modules/@vue/typescript-plugin'
+            )
 
-                local mason_lspconfig = require 'mason-lspconfig'
-
-                mason_lspconfig.setup()
-
-                local lspconfig = require 'lspconfig'
-
-                mason_lspconfig.setup_handlers {
-                    function(server)
-                        lspconfig[server].setup {}
-                    end,
-                    ['ts_ls'] = function()
-                        local vue_ts_plugin =
-                            require 'mason-registry'
-                            .get_package 'vue-language-server'
-                            :get_install_path() ..
-                            '/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin'
-
-                        lspconfig['ts_ls'].setup {
-                            init_options = {
-                                plugins = {
-                                    {
-                                        name = '@vue/typescript-plugin',
-                                        location = vue_ts_plugin,
-                                        languages = {
-                                            'javascript',
-                                            'typescript',
-                                            'vue'
-                                        }
-                                    }
-                                }
+            vim.lsp.config('ts_ls', {
+                init_options = {
+                    plugins = {
+                        {
+                            name = '@vue/typescript-plugin',
+                            location = vue_ts_plugin,
+                            languages = {
+                                'javascript',
+                                'typescript',
+                                'vue'
                             }
                         }
+                    }
+                }
+            })
+
+            require 'mason'.setup()
+            require 'mason-lspconfig'.setup {
+                ensure_installed = {
+                    'ts_ls'
+                }
+            }
+
+            vim.api.nvim_create_augroup('Linting', { clear = true })
+            vim.api.nvim_create_autocmd(
+                { 'BufEnter', 'BufWritePost', 'InsertLeave' },
+                {
+                    group = 'Linting',
+                    callback = function()
+                        require 'lint'.try_lint()
                     end
                 }
-            end
+            )
 
-            local function setup_linter()
-                vim.api.nvim_create_augroup('Linting', { clear = true })
-                vim.api.nvim_create_autocmd(
-                    { 'BufEnter', 'BufWritePost', 'InsertLeave' },
-                    {
-                        group = 'Linting',
-                        callback = function()
-                            require 'lint'.try_lint()
-                        end
-                    }
-                )
+            require 'lint'.linters_by_ft = {
+                javascript = { 'eslint_d' },
+                typescript = { 'eslint_d' },
+                python = { 'pylint' },
+                html = { 'tidy' },
+                lua = { 'luacheck' },
+                markdown = { 'markdownlint' },
+                c = { 'clangtidy' },
+                tex = { 'lacheck' },
+                latex = { 'lacheck' }
+            }
 
-                require 'lint'.linters_by_ft = {
-                    javascript = { 'eslint_d' },
-                    typescript = { 'eslint_d' },
-                    python = { 'pylint' },
-                    html = { 'tidy' },
-                    lua = { 'luacheck' },
-                    markdown = { 'markdownlint' },
-                    c = { 'clangtidy' },
-                    tex = { 'lacheck' },
-                    latex = { 'lacheck' }
-                }
-
-                require 'mason-nvim-lint'.setup {
-                    automatic_installation = false
-                }
-            end
-
-            setup_lsp()
-            setup_linter()
+            require 'mason-nvim-lint'.setup {
+                automatic_installation = false
+            }
         end
     },
     {
