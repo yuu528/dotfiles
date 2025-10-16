3b4e1c3f11de07ee1a4dea68c39b29e9fa68f214
Yuu528 <yuu528mail@gmail.com>
2025-10-16 09:18:56 +0900

    Neovim: Add toggleterm.nvim

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -127,6 +127,43 @@ return {
         end
     },
     {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        keys = {
+            { '<LEADER>te' }
+        },
+        config = function()
+            require 'toggleterm'.setup {
+                size = function(term)
+                    if term.direction == 'horizontal' then
+                        return 24
+                    elseif term.direction == 'vertical' then
+                        return vim.o.columns * 0.4
+                    end
+                end,
+                open_mapping = [[<LEADER>te]]
+            }
+
+            local powershell_options = {
+                shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+                shellcmdflag =
+                "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+                shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+                shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+                shellquote = "",
+                shellxquote = "",
+            }
+
+            for option, value in pairs(powershell_options) do
+                vim.opt[option] = value
+            end
+        end,
+        opts = {
+            open_mapping = [[<LEADER>te]],
+            hide_numbers = false,
+        }
+    },
+    {
         'lervag/vimtex',
         lazy = false,
         ft = { 'tex', 'ltx' },
