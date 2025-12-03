b76fa2156153c69dc773e8c63ac16a1bebb73730
Yuu528 <yuu528mail@gmail.com>
2025-12-03 09:23:02 +0900

    Neovim: Fix shell option

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -147,17 +147,19 @@ return {
 				open_mapping = [[<LEADER>te]],
 			})
 
-			local powershell_options = {
-				shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-				shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-				shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-				shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-				shellquote = "",
-				shellxquote = "",
-			}
+			if vim.fn.executable("pwsh") == 1 or vim.fn.executable("powershell") == 1 then
+				local powershell_options = {
+					shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+					shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+					shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+					shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+					shellquote = "",
+					shellxquote = "",
+				}
 
-			for option, value in pairs(powershell_options) do
-				vim.opt[option] = value
+				for option, value in pairs(powershell_options) do
+					vim.opt[option] = value
+				end
 			end
 		end,
 		opts = {
