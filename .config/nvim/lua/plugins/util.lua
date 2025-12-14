285bad59c81e707be017bf183721a9e77e94b90f
Yuu528 <yuu528mail@gmail.com>
2025-12-14 15:13:25 +0900

    Neovim: mini.files: Disable :w

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -66,23 +66,6 @@ return {
 			local mini_files = require("mini.files")
 
 			mini_files.setup()
-
-			-- Use ':w' to synchronize
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "MiniFilesBufferCreate",
-				callback = function(ev)
-					vim.schedule(function()
-						vim.api.nvim_set_option_value("buftype", "acwrite", { buf = 0 })
-						vim.api.nvim_buf_set_name(0, tostring(vim.api.nvim_get_current_win()))
-						vim.api.nvim_create_autocmd("BufWriteCmd", {
-							buffer = ev.data.buf_id,
-							callback = function()
-								mini_files.synchronize()
-							end,
-						})
-					end)
-				end,
-			})
 		end,
 	},
 	{
